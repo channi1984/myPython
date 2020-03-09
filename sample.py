75983f8f64b325019e4f1eab41ce8ea40de29646
Son Dong Chan <channi.kr@gmail.com>
2020-03-09 10:53:57 +0900

    a

---
+++ sample.py
@@ -0,0 +1,25 @@
+# number = input("정수 입력>")
+# number = int(number)
+
+# #양수 조건
+# if number > 0:
+# 	print("양수입니다")
+
+# #음수 조건
+# if number < 0:
+# 	print("음수입니다.")
+
+# # 0 조건
+# if number == 0:
+# 	print("0입니다.")
+
+# i = 0
+# while i <= 10:
+# 	print(i)
+# 	i+=1
+
+# sum = 0
+# for i in range(11):
+# 	sum += i
+# print(sum)
+
