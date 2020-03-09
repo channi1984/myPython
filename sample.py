edc25dbf3681052a9cc8fa1b2db8b597e9811a09
Son Dong Chan <channi.kr@gmail.com>
2020-03-09 11:53:25 +0900

    a

--- sample.py
+++ sample.py
@@ -23,3 +23,47 @@
 # 	sum += i
 # print(sum)
 
+# list = ["This", "is", "a", "book"]
+# for s in list:
+# 	print(s)
+
+# numbers = range(2,11,2)
+
+# for x in numbers:
+# 	print(x)
+
+# a = ["AB", 0, False]
+# a.append(21.5)
+# a[1] = 11
+# del a[2]
+# print(a)
+
+# a= [1,2]
+# b= [3,4,5]
+# c= a+b
+# print(c)
+# d= a*3
+# print(d)
+
+# mylist = "This is a book That is pencil".split()
+# i = mylist.index('book')
+# n = mylist.count('is')
+# print(i,n)
+
+# scores = {"철수":90, "민수":80, "영희":80}
+# v = scores["민수"]
+# scores["민수"] = 88
+# print(v)
+
+# scores = {"철수":90, "민수":80, "영희":80}
+# for key in scores:
+# 	val = scores[key]
+# 	print(key, val)
+
+
+# myset = {1,1,3,5,5}
+# print(myset)
+
+# mylist = ["A","A","B","B","B",]
+# s = set(mylist)
+# print(s)
