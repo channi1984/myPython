9c4e14cb93470be654e0bbc31cd796039d2da14e
Son Dong Chan <channi.kr@gmail.com>
2020-03-09 13:21:15 +0900

    1

--- sample.py
+++ sample.py
@@ -67,3 +67,37 @@
 # mylist = ["A","A","B","B","B",]
 # s = set(mylist)
 # print(s)
+
+
+# myset = {1,3,5}
+
+# myset.add(7)
+# print(myset)
+
+# myset.update({4,2,10})
+# print(myset)
+
+# myset.remove(1)
+# print(myset)
+
+# myset.clear()
+# print(myset)
+
+
+# a = {1, 3, 5}
+# b = {1, 2, 5}
+
+# i = a & b
+# print(i)
+
+# u = a | b
+# print(u)
+
+# d = a - b
+# print(d)
+
+def sum(a,b):
+	s = a+b
+	return s
+total = sum(4,6)
+print(total)
